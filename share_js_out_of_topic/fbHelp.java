5b064f26b64c1862106634e43a5964ea89aedb0f
Kiran Pantha <info@kiranpantha.com.np>
2020-04-25 00:46:28 +0545

    Create fbHelp.java

---
+++ share_js_out_of_topic/fbHelp.java
@@ -0,0 +1,40 @@
+// java Program to create a simple JList 
+import java.awt.event.*;
+import java.text.DecimalFormat;
+import java.awt.*; 
+import javax.swing.*; 
+import java.util.ArrayList;
+import java.util.List;
+
+class solve extends JFrame  
+{ 
+
+    static JFrame f; 
+    static JList oceanLevel; 
+    private static DecimalFormat df2;
+    public static void main(String[] args) 
+    { 
+        f = new JFrame("Ocean level"); 
+        JPanel p =new JPanel(); 
+        df2 = new DecimalFormat("#.##");
+        List<String> oceanLevelData = new ArrayList<String>();
+        double oldOceanVal = 0.0;
+        for(int i = 1; i<= 10 ; i++)
+        {
+            double douFormula = Math.pow(1.5, i-1);
+            oceanLevelData.add(String.valueOf(df2.format(oldOceanVal + douFormula)));
+            oldOceanVal = oldOceanVal + douFormula;
+        }
+        
+        //create list 
+        oceanLevel = new JList(oceanLevelData.toArray());   
+        oceanLevel.setSelectedIndex(2); 
+        p.add(oceanLevel); 
+        oceanLevel.setPreferredSize(new Dimension(180, 400));
+        f.add(p); 
+        f.setSize(200,400); 
+        f.show(); 
+    } 
+      
+      
+} 
